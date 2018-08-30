3a7302c9fcb4dfe1a7cb841a95246674d0950cbd
Jean Carlos Azevedo de Souza <Jean.Carlos.Azevedo@ibm.com>
2018-08-30 11:40:14 -0300

    fix: changes code of server, changes passport and jwt

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const _ = require("lodash");
+var _ = require("lodash");
+var express = require("express");
+var bodyParser = require("body-parser");
+var jwt = require('jsonwebtoken');
 
-const app = express();
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
 
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const passportJWT = require('passport-jwt');
-
-const ExtractJwt = passportJWT.ExtractJwt;
-const JwtStrategy = passportJWT.Strategy;
-
-app.use(passport.initialize());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-var jwtOptions = {}
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
-jwtOptions.secretOrKey = 'tasmanianDevil';
-
-var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-  console.log('payload received', jwt_payload);
-  var user = users[_.findIndex(users, { id: jwt_payload.id })];
-  if (user) {
-    next(null, user);
-  } else {
-    next(null, false);
-  }
-});
-
-passport.use(strategy);
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
 
 var users = [
   {
@@ -44,30 +22,71 @@ var users = [
   }
 ];
 
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  // usually this would be a database call:
+  var user = users[_.findIndex(users, { id: jwt_payload.id })];
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
+var app = express();
+app.use(passport.initialize());
+
+// parse application/x-www-form-urlencoded
+// for easier testing with Postman or plain HTML forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/", function (req, res) {
+  res.json({ message: "Express is up!" });
+});
+
 app.post("/login", function (req, res) {
   if (req.body.name && req.body.password) {
     var name = req.body.name;
     var password = req.body.password;
   }
+  // usually this would be a database call:
   var user = users[_.findIndex(users, { name: name })];
   if (!user) {
     res.status(401).json({ message: "no such user found" });
   }
 
-  if (password === user.password) {
-    var payload = { id: req.body.id };
+  if (user.password === req.body.password) {
+    // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+    var payload = { id: user.id };
     var token = jwt.sign(payload, jwtOptions.secretOrKey);
-    res.send({ message: "ok", token: token });
+    res.json({ message: "ok", token: token });
   } else {
     res.status(401).json({ message: "passwords did not match" });
   }
 });
 
-app.get('/secret', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.redirect('/testingWorked')
+app.get("/secret", passport.authenticate('jwt', { session: false }), function (req, res) {
+  res.json({ message: "Success! You can not see this without a token" });
 });
+
 
 require('./server/routes')(app);
 require('./server/cors')(app);
+
+app.listen(9000, function () {
+  console.log("Express running");
+});
 
 module.exports = app;
